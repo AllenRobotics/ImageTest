a3f8420e0fe7fe2dbd33729f902528926018a87b
Eagle Robotics <team5417@gmail.com>
2017-01-14 13:53:44 -0600

    Added place holder for where we want to filter out colors

--- src/org/usfirst/frc/team5417/robot/ImageTest.java
+++ src/org/usfirst/frc/team5417/robot/ImageTest.java
@@ -33,8 +33,33 @@ public class ImageTest {
 	    MatrixUtilities.LoadOpenCVLibraries();
 	   
 	      
-         Mat newMat = MatrixUtilities.readMatFromFile("C:/temp/sampleimage.jpg");
-         
+        Mat newMat = MatrixUtilities.readMatFromFile("C:/temp/sampleimage.jpg");
+        
+        
+        
+        //
+        // TODO: filter all colors except a range from the newMat
+        //
+        
+        // here is a sample range for each color channel
+        // TODO: find a real color range
+        byte minRed = (byte)100, maxRed = (byte)150;
+        byte minGreen = (byte)50, maxGreen = (byte)100;
+        byte minBlue = (byte)10, maxBlue = (byte)70;
+        
+        for (int r = 0; r < newMat.rows(); ++r)
+        {
+        	for (int c = 0; c < newMat.cols(); ++c)
+        	{
+        		byte[] rgb = new byte[3];
+        		newMat.get(r, c, rgb);
+        		
+        		// TODO: right here, if the rgb values for this pixel are in the range, do nothing.
+        		//       if the rgb values for this pixel are outside the range, make the pixel black
+        		
+        	}
+        }
+        
          
 		BufferedImage outputImage = MatrixUtilities.mat2Img(newMat);
 		File outputfile = new File("C:/temp/outputImage.jpg");
